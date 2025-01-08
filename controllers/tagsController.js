1bb29f7d5a8ba757702d6dcaf827e58a3b1762fb
federicascognamiglio <scognamigliofede99@gmail.com>
2025-01-08 15:56:28 +0100

    feat: tags endpoint fixed

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -5,7 +5,7 @@ const index = (req, res) => {
 
     for (let i = 0; i < posts.length; i++ ) {
         const postTags = posts[i].tags;
-        for (let j = 0; j < postTags.length; i++) {
+        for (let j = 0; j < postTags.length; j++) {
             const curTag = postTags[j];
             if(!tags.includes(curTag)) {
                 tags.push(curTag);
